d01e72bcba88ad3ba6ba13be944aad4b2e73b356
hgpark <digitalworks_hgpark@amorepacific.com>
2024-11-07 16:28:25 +0900

    회원가입 데이터 로컬스토리지에 저장

--- js/sign/signStep4.js
+++ js/sign/signStep4.js
@@ -1,9 +1,17 @@
 import { signInfo } from "./sign.js";
 
+const saveSigndataLocal = () => {
+  localStorage.setItem("userData", JSON.stringify(signInfo));
+  location.reload(true);
+}
+
 export const signStep4 = () => {
   const ModalTitle = document.querySelector(".ModalTitle");
   ModalTitle.innerHTML = "";
-  document.querySelector(".ResultProfile img").src= `/assets/images/${signInfo.profile}`;
+  document.querySelector(".ResultProfile img").src = `/assets/images/${signInfo.profile}`;
   document.querySelector(".ResultTitle span").innerText = signInfo.nickname;
   document.querySelector(".ResultProfile").style.backgroundColor = signInfo.profileBg;
+  document.querySelector(".ModalBg").addEventListener("click", saveSigndataLocal);
+  document.querySelector(".ModalClose").addEventListener("click", saveSigndataLocal);
+  document.querySelector("button.Sign4").addEventListener("click", saveSigndataLocal);
 }
