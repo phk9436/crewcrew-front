cc438c397cb329a254897dab4c4a3f8a155751c4
hgpark <digitalworks_hgpark@amorepacific.com>
2024-11-11 13:30:12 +0900

    게시물목록 더미데이터로 렌더링하도록 수정

---
+++ js/post/postData.js
@@ -0,0 +1,163 @@
+const getDummyEndDate = (n) => {
+  //임의로 n일 뒤에 마감되는 데이터 세팅하기 위함
+  const date = new Date();
+  const endDate = date;
+  endDate.setDate(date.getDate() + n);
+  const year = endDate.getFullYear();
+  const month = `${endDate.getMonth() + 1}`.padStart(2, '0');
+  const day = `${endDate.getDate()}`.padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+export let postData = [
+  {
+    id: 10,
+    dateDiff: '2',
+    endDate: getDummyEndDate(2),
+    title: "함께 크루원 모집 플랫폼 작업하실 분 모십니다!",
+    category: "Study",
+    categoryName: "프로젝트",
+    place: "온라인",
+    nowPop: 9,
+    fullPop: 10,
+    read: 50,
+    profile: "Profile4.png",
+    profileBg: "#8d2bf5",
+    nickname: "고슴도치"
+  },
+  {
+    id: 9,
+    dateDiff: '2',
+    endDate: getDummyEndDate(2),
+    title: "일본어 스터디 해요!",
+    category: "Study",
+    categoryName: "어학",
+    place: "오프라인",
+    nowPop: 7,
+    fullPop: 8,
+    read: 50,
+    profile: "Profile1.png",
+    profileBg: "#00b7ff",
+    nickname: "일본어덕후"
+  },
+  {
+    id: 8,
+    dateDiff: '3',
+    endDate: getDummyEndDate(3),
+    title: "요리 좋아하시는 분?",
+    category: "Hobby",
+    categoryName: "요리",
+    place: "오프라인",
+    nowPop: 1,
+    fullPop: 4,
+    read: 50,
+    profile: "Profile2.png",
+    profileBg: "#8ed819",
+    nickname: "백종원"
+  },
+  {
+    id: 7,
+    dateDiff: '4',
+    endDate: getDummyEndDate(4),
+    title: "한강 러닝 뛰실분 모집합니다",
+    category: "Hobby",
+    categoryName: "운동",
+    place: "오프라인",
+    nowPop: 5,
+    fullPop: 10,
+    read: 50,
+    profile: "Profile2.png",
+    profileBg: "#8ed819",
+    nickname: "러닝러버"
+  },
+  {
+    id: 6,
+    dateDiff: '4',
+    endDate: getDummyEndDate(4),
+    title: "취준 스터디 하실분 구해요",
+    category: "Study",
+    categoryName: "취업",
+    place: "오프라인",
+    nowPop: 3,
+    fullPop: 5,
+    read: 50,
+    profile: "Profile3.png",
+    profileBg: "#ff0045",
+    nickname: "취뽀하자"
+  },
+  {
+    id: 5,
+    dateDiff: '4',
+    endDate: getDummyEndDate(4),
+    title: "덕질 모임!",
+    category: "Hobby",
+    categoryName: "덕질",
+    place: "온라인",
+    nowPop: 4,
+    fullPop: 5,
+    read: 50,
+    profile: "Profile4.png",
+    profileBg: "#8d2bf5",
+    nickname: "덕질최고"
+  },
+  {
+    id: 4,
+    dateDiff: '6',
+    endDate: getDummyEndDate(6),
+    title: "5인랭 하실분",
+    category: "Hobby",
+    categoryName: "게임",
+    place: "온라인",
+    nowPop: 3,
+    fullPop: 5,
+    read: 50,
+    profile: "Profile3.png",
+    profileBg: "#ff0045",
+    nickname: "롤선생"
+  },
+  {
+    id: 3,
+    dateDiff: '7',
+    endDate: getDummyEndDate(7),
+    title: "공무원 준비는 크루크루!",
+    category: "Study",
+    categoryName: "고시/공무원",
+    place: "오프라인",
+    nowPop: 4,
+    fullPop: 10,
+    read: 50,
+    profile: "Profile5.png",
+    profileBg: "#ffd458",
+    nickname: "고시생1"
+  },
+  {
+    id: 2,
+    dateDiff: '7',
+    endDate: getDummyEndDate(7),
+    title: "요즘 트렌드는 크루크루!",
+    category: "Hobby",
+    categoryName: "트렌드",
+    place: "온라인",
+    nowPop: 1,
+    fullPop: 3,
+    read: 50,
+    profile: "Profile1.png",
+    profileBg: "#00b7ff",
+    nickname: "크루크루"
+  },
+  {
+    id: 1,
+    dateDiff: '8',
+    endDate: getDummyEndDate(8),
+    title: "맛집탐방해요!",
+    category: "Hobby",
+    categoryName: "기타",
+    place: "오프라인",
+    nowPop: 2,
+    fullPop: 4,
+    read: 50,
+    profile: "Profile4.png",
+    profileBg: "#8d2bf5",
+    nickname: "먹는게최고야"
+  },
+]
--- js/post/postlist.js
+++ js/post/postlist.js
@@ -1,3 +1,5 @@
+import { postData } from "./postData.js";
+
 window.addEventListener('DOMContentLoaded', function () {
 
   const FilterWrap = document.querySelector('.FilterWrapper');
@@ -39,4 +41,48 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+
+  console.log(postData)
+  const PostCont = document.querySelector(".PostWrapper ul");
+  let postList = ``;
+  postData.forEach((e) => {
+    const endDate = `${e.endDate.split("-")[1]}/${e.endDate.split("-")[2]}`;
+    const days = ['월', '화', '수', '목', '금', '토', '일'];
+    const endDay = days[new Date(e.endDate).getDay()];
+    postList += /*html*/ `
+    <li data-id="${e.id}">
+      <div class="PostCard">
+        <div class="PostCardHead">
+          <div class="ProfileBox" style="background-color:${e.profileBg}">
+            <img src="/assets/images/${e.profile}" alt="">
+          </div>
+          <div class="TextBox">
+            <p class="Dday">D-${e.dateDiff}</p>
+            <p class="Date">${endDate} (${endDay})</p>
+            <p class="Name">${e.nickname}</p>
+          </div>
+        </div>
+        <div class="PostCardBody">
+          <div class="TextBox">
+            <div class="TitleBox">
+              <h5>${e.title}</h5>
+              <div class="Star"></div>
+            </div>
+            <div class="TextList">
+              <p class="Category ${e.category}">${e.categoryName}</p>
+              <p>${e.place}</p>
+              <p>${e.nowPop}/${e.fullPop}명</p>
+              <p>조회수 ${e.read}</p>
+            </div>
+          </div>
+          <div class="ButtonBox">
+            <button class="Detail">상세보기</button>
+            <button class="Participate">참여하기</button>
+          </div>
+        </div>
+      </div>
+    </li>
+    `;
+  });
+  PostCont.innerHTML = postList;
 });
