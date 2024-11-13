de8029bef57ce3f78f58d3d85427dd074d348241
hgpark <digitalworks_hgpark@amorepacific.com>
2024-11-13 13:58:15 +0900

    게시글 작성 모달 ui 추가

---
+++ js/modal/postmodal.js
@@ -0,0 +1,185 @@
+import { postData } from "../post/postData.js";
+
+const postmodal = /*html*/ `
+  <div class="ModalBg"></div>
+
+  <div class="ModalBoxPost">
+    <div class="ModalPostBody">
+      <div class="HeaderContent">
+        <div>
+          <h4>카테고리</h4>
+          <ul class="PostListFlex">
+            <li>
+              <input type="radio" class="InputHide InputPostRadio" name="PostCat" id="PostStudy" checked>
+              <label for="PostStudy" class="ButtonPost">스터디</label>
+            </li>
+            <li>
+              <input type="radio" class="InputHide InputPostRadio" name="PostCat" id="PostHobby">
+              <label for="PostHobby" class="ButtonPost">취미</label>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>상세 카테고리</h4>
+          <ul class="PostListDrop PostListCatDet">
+            <li>
+              <input type="text" class="InputPostCatDet InputChoose" autocomplete="off" readonly
+                placeholder="어떤 스터디 크루원이 필요하세요?">
+              <span class="Arrow"></span>
+            </li>
+            <li>
+              <input type="radio" class="InputHide" name="PostCatDet" id="DetStudy1" value="어학">
+              <label for="DetStudy1" class="LabelChoose">
+                <p class="Choose"><em>어학</em>(토플/토익)</p>
+              </label>
+            </li>
+            <li>
+              <input type="radio" class="InputHide" name="PostCatDet" id="DetStudy2" value="취업">
+              <label for="DetStudy2" class="LabelChoose">
+                <p class="Choose"><em>취업</em>(면접/자소서)</p>
+              </label>
+            </li>
+            <li>
+              <input type="radio" class="InputHide" name="PostCatDet" id="DetStudy3" value="고시/공무원">
+              <label for="DetStudy3" class="LabelChoose">
+                <p class="Choose"><em>고시/공무원</em></p>
+              </label>
+            </li>
+            <li>
+              <input type="radio" class="InputHide" name="PostCatDet" id="DetStudy4" value="프로젝트">
+              <label for="DetStudy4" class="LabelChoose">
+                <p class="Choose"><em>프로젝트</em>(디자인/개발)</p>
+              </label>
+            </li>
+            <li>
+              <input type="radio" class="InputHide" name="PostCatDet" id="DetStudy5" value="기타">
+              <label for="DetStudy5" class="LabelChoose">
+                <p class="Choose"><em>기타</em></p>
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="HeaderContent">
+        <div>
+          <h4>모임방식</h4>
+          <ul class="PostListFlex">
+            <li>
+              <input type="radio" class="InputHide InputPostRadio" name="PostOnline" id="PostOnline" checked>
+              <label for="PostOnline" class="ButtonPost">온라인</label>
+            </li>
+            <li>
+              <input type="radio" class="InputHide InputPostRadio" name="PostOnline" id="PostOffline">
+              <label for="PostOffline" class="ButtonPost">오프라인</label>
+            </li>
+          </ul>
+        </div>
+        <div class="ContentFlex">
+          <div>
+            <h4>모집 인원수</h4>
+            <ul class="PostListDrop PostListPeople">
+              <li>
+                <input type="number" class="InputPostPeople InputChoose" value="1" max="10">
+                <span class="ArrowNum Up"></span>
+                <span class="ArrowNum Down"></span>
+              </li>
+              <li>
+                <span class="num">1</span>
+                <span class="num">2</span>
+                <span class="num">3</span>
+                <span class="num">4</span>
+                <span class="num">5</span>
+                <span class="num">6</span>
+                <span class="num">7</span>
+                <span class="num">8</span>
+                <span class="num">9</span>
+                <span class="num">10</span>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4>마감일자</h4>
+            <input type="date" value="2022-03-08" class="InputFull InputDate">
+          </div>
+        </div>
+      </div>
+      <div class="ToolTipWrapper">
+        <h4>*초대링크 (필수) <span class="ToolTip">?</span></h4>
+        <div class="ToolTipDt">
+          모집글에 지원하는 작성자가 합격 후 참여할 수 있는 채팅방의 링크를 생성 후 이곳에 기입해주세요! 가입한 링크는 합격자에게만 보여집니다.<br>예시) 카카오톡 오픈채팅방 링크
+        </div>
+        <div class="InputBox">
+          <input type="text" class="InputFull InputTitle">
+          <div class="InputDel"></div>
+        </div>
+      </div>
+
+      <h4>제목</h4>
+      <div class="InputBox">
+        <input type="text" class="InputFull InputTitle">
+        <div class="InputDel"></div>
+      </div>
+
+      <h4>내용</h4>
+      <div class="TextArea">
+        <textarea name="" id=""></textarea>
+      </div>
+    </div>
+
+    <div class="ModalPostFooter">
+      <ul class="PostListFlex">
+        <li>
+          <button type="button" class="ButtonPost ButtonPostCancle">취소</button>
+        </li>
+        <li>
+          <button type="submit" class="ButtonPost">업로드</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body");
+  let isCreated = false;
+  const creatPostmodal = () => {
+    if (!isCreated) {
+      const ModalWrapper = document.createElement("div");
+      ModalWrapper.classList.add("ModalWrapper");
+      ModalWrapper.innerHTML = postmodal;
+      body.append(ModalWrapper);
+      body.classList.add("Modal");
+      document.querySelector(".ModalWrapper").style.display = "flex";
+      setTimeout(() => {
+        document.querySelector(".ModalWrapper").classList.add("Modal");
+      }, 10);
+      isCreated = true;
+      document.querySelector(".ModalBg").addEventListener("click", removeModal);
+      document.querySelector(".ButtonPostCancle").addEventListener("click", removeModal);
+    }
+    document.querySelector(".ModalWrapper").style.display = "flex";
+    body.classList.add("Modal");
+    setTimeout(() => {
+      document.querySelector(".ModalWrapper").classList.add("Modal");
+    }, 10);
+  };
+  const removeModal = () => {
+    body.classList.remove("Modal");
+    document.querySelector(".ModalWrapper").classList.remove("Modal");
+    setTimeout(() => {
+      document.querySelector(".ModalWrapper").style.display = "none";
+    }, 500);
+  };
+  const openPostmodal = (e) => {
+    const isLogin = sessionStorage.getItem("isLogin");
+    e.preventDefault();
+    if (!isLogin) {
+      alert("로그인이 필요합니다.");
+      return;
+    }
+    creatPostmodal();
+  };
+  document.querySelectorAll(".createButton").forEach((e) => e.addEventListener("click", openPostmodal));
+  document.querySelectorAll(".WriteButton").forEach((e) => e.addEventListener("click", openPostmodal));
+});
